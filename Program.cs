8752be1a03a0e3a623d4aa0694f4933ee23deef4
alejandrio.vasay <alejandrio.vasay@tccd.edu>
2021-08-31 14:25:14 -0500

    Changed where I call ReadKey in the loop

--- Program.cs
+++ Program.cs
@@ -1,21 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-// Initialize our list
-List<string> mylist = new List<string>(new string[] { "biology-1", "biology-2", "biology-3" });
-
-ConsoleKeyInfo consoleKeyInfo;
-
-Random R = new Random();
-Console.WriteLine("Press Enter key to continue | Escape key to quit"); 
-
-
-do
+public class Program
 {
-    // get random number from 0 to 2. 
-    int someRandomNumber = R.Next(0, mylist.Count());
+    public static void Main()
+    {
 
-    Console.WriteLine("Hello {0}", mylist.ElementAt(someRandomNumber));
+        // See https://aka.ms/new-console-template for more information
 
-    consoleKeyInfo = Console.ReadKey();
+        // Initialize our list
+        List<string> mylist = new List<string>(new string[] { "biology-1", "biology-2", "biology-3" });
 
-} while(consoleKeyInfo.Key != ConsoleKey.Escape);
+
+        Random R = new Random();
+        Console.WriteLine("Press Enter key to continue | Escape key to quit");
+
+        ConsoleKeyInfo k = Console.ReadKey();
+
+        while (k.Key != ConsoleKey.Escape)
+        {
+            // get random number from 0 to 2. 
+            int someRandomNumber = R.Next(0, mylist.Count);
+
+            Console.WriteLine("Hello {0}", mylist.ElementAt(someRandomNumber));
+
+            k = Console.ReadKey();
+
+        }
+
+    }
+}
