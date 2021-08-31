d174d6b63d10faf9332056658a609b70c594cf8a
alejandrio.vasay <alejandrio.vasay@tccd.edu>
2021-08-31 11:57:10 -0500

    First push - .NET 6 version of GetRandomElementFromList

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Initialize our list
+List<string> mylist = new List<string>(new string[] { "biology-1", "biology-2", "biology-3" });
+
+ConsoleKeyInfo consoleKeyInfo;
+
+Random R = new Random();
+Console.WriteLine("Press Enter key to continue | Escape key to quit");
+
+do
+{
+    // get random number from 0 to 2. 
+    int someRandomNumber = R.Next(0, mylist.Count());
+
+    Console.WriteLine("Hello {0}", mylist.ElementAt(someRandomNumber));
+
+    consoleKeyInfo = Console.ReadKey();
+
+} while(consoleKeyInfo.Key != ConsoleKey.Escape);
